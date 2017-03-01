4e55fd1a92dcc50c66cb19ce9a2d79ab19fac54c
Max Gurewitz <maxthegeek1@gmail.com>
2017-02-28 22:23:32 -0800

    move print statement into bin

--- bin/search-har.js
+++ bin/search-har.js
@@ -1,1 +1,10 @@
-require('../src/search-har')();
+const searchHar = require('../src/search-har');
+const util = require('util');
+
+searchHar((err, res) => {
+  if (err) {
+    console.error('Cannot connect to Chrome: ' + err);
+  } else {
+    console.log('Done', util.inspect(res, { depth: null, colors: true }));
+  }
+});
--- src/search-har.js
+++ src/search-har.js
@@ -1,24 +1,29 @@
 const harCapturer = require('chrome-har-capturer');
-const util = require('util');
 
-module.exports = function searchHar() {
-  // TODO: make dateTime dynamic
-  // TODO: add variable number of requests
+// TODO: make dateTime dynamic
+// TODO: add variable number of requests
+module.exports = function searchHar(cb) {
+  let done = false;
+
   const capturerInstance =
     harCapturer.load([
-      'https://www.opentable.com/s/?covers=2&dateTime=2017-02-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0',
-      'https://www.opentable.com/s/?covers=2&dateTime=2017-02-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0'
+      'https://www.opentable.com/s/?covers=2&dateTime=2017-04-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0',
+      'https://www.opentable.com/s/?covers=2&dateTime=2017-04-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0'
     ]);
 
-  capturerInstance.on('connect', () => {
-    console.log('Connected to Chrome');
-  });
+  capturerInstance.on('end', hars => {
+    const res = hars;
 
-  capturerInstance.on('end', har => {
-    console.log('hars downloaded', util.inspect(har, { depth: null, colors: true }));
+    if (!done) {
+      done = true;
+      cb(null, res);
+    }
   });
 
   capturerInstance.on('error', err => {
-    console.error('Cannot connect to Chrome: ' + err);
+    if (!done) {
+      done = true;
+      cb(err);
+    }
   });
 };
