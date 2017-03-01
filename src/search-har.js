46e806cdb6566ddfccfa94ad1e8d5d1349e51571
Max Gurewitz <maxthegeek1@gmail.com>
2017-02-28 19:24:59 -0800

    add todo

--- src/search-har.js
+++ src/search-har.js
@@ -1,21 +1,24 @@
-const chc = require('chrome-har-capturer');
+const harCapturer = require('chrome-har-capturer');
 const util = require('util');
 
 module.exports = function searchHar() {
-  const c =
-    chc.load([
+  // TODO: make dateTime dynamic
+  // TODO: add variable number of requests
+  const capturerInstance =
+    harCapturer.load([
+      'https://www.opentable.com/s/?covers=2&dateTime=2017-02-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0',
       'https://www.opentable.com/s/?covers=2&dateTime=2017-02-28%2019%3A00&metroId=4&regionIds=5&enableSimpleCuisines=true&pageType=0'
     ]);
 
-  c.on('connect', () => {
+  capturerInstance.on('connect', () => {
     console.log('Connected to Chrome');
   });
 
-  c.on('end', har => {
+  capturerInstance.on('end', har => {
     console.log('hars downloaded', util.inspect(har, { depth: null, colors: true }));
   });
 
-  c.on('error', err => {
+  capturerInstance.on('error', err => {
     console.error('Cannot connect to Chrome: ' + err);
   });
 };
